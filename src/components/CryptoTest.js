2177fed50685bd9bea815f398eca0ea160ce3492
Don Romaniello <don.romaniello@gmail.com>
2022-05-13 15:20:26 -0400

    Broken working.

--- src/components/CryptoTest.js
+++ src/components/CryptoTest.js
@@ -1,42 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Web3 from "web3";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { providers } from "ethers";
 
 let provider, web3, web3Provider;
 
-const startUp = async () => {
-  provider = new WalletConnectProvider({
-    infuraId: "6237838e24b74f8bb53f0cb090a0244d", // Required
-    qrcodeModalOptions: {
-      mobileLinks: [
-        "rainbow",
-        "metamask",
-        "argent",
-        "trust",
-        "imtoken",
-        "pillar",
-      ],
-    },
-  });
-  console.log("provider is ", provider);
-
-  //  Enable session (triggers QR Code modal)
-  await provider.enable();
-
-  //  Create Web3
-  web3 = new Web3(provider);
-  //   web3Provider = new providers.Web3Provider(provider);
-  web3.eth.getAccounts().then(console.log);
-  console.log("wallet is ", web3.eth.accounts.wallet);
-};
-
 export const CryptoTest = () => {
+
+  const startUp = async () => {
+    provider = new WalletConnectProvider({
+      infuraId: "6237838e24b74f8bb53f0cb090a0244d", // Required
+      qrcodeModalOptions: {
+        mobileLinks: [
+          "rainbow",
+          "metamask",
+          "argent",
+          "trust",
+          "imtoken",
+          "pillar",
+        ],
+      },
+    });
+    console.log("provider is ", provider);
+
+    //  Enable session (triggers QR Code modal)
+    await provider.enable();
+
+    //  Create Web3
+    web3 = new Web3(provider);
+    //   web3Provider = new providers.Web3Provider(provider);
+    web3.eth.getAccounts().then(console.log);
+    console.log("wallet is ", web3.eth.accounts.wallet);
+
+    return(web3);
+
+  };
+
   const [address, setAddress] = useState("");
+
+  const [web3, setWeb3] = useState();
+
+  useEffect(() => {
+
+    console.log("reaching effect on 42")
+
+    if (!web3) {
+      console.log("Out of reach")
+      setWeb3(startUp());
+      setAddress(provider.accounts[0])
+    }
+
+    console.log("Always in reach")
+  }, [])
+
+
+
+
   return (
     <div>
       <h2>Your Address: {address}</h2>
-      <button
+      {!web3 ? <button
         onClick={async () => {
           startUp();
           const accounts = provider.accounts;
@@ -45,7 +68,7 @@ export const CryptoTest = () => {
         }}
       >
         Connect
-      </button>
+      </button> : ''}
       {address.length ? (
         <button
           onClick={async () => {
