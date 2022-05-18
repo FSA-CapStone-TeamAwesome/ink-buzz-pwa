db7b974f7df974a3ae30948108715b89b92d459e
JRC91 <jimcarmody91@gmail.com>
2022-05-18 15:55:54 -0400

    all good

--- src/components/FollowedArtists.js
+++ src/components/FollowedArtists.js
@@ -41,17 +41,20 @@ const FollowedArtists = () => {
   return (
     <div>
       <h1>Here's the latest from your favorites.</h1>
-      <h2>A Quick Five from your Faves</h2>
+      <h2>A Quick Five from your Faves</h2> <br />
       {following.map((coolDude) => {
         return (
-          <div className="d-flex flex-wrap justify-content-evenly align-items-center">
-            <h2>{coolDude.name}</h2>
+          <div>
+            <h2>{coolDude.name}</h2><br />
+
             {coolDude.images ? (
+               <div className="d-flex flex-wrap justify-content-evenly align-items-center">{
               coolDude.images.map((link, index) => {
                 if(index >= coolDude.images.length-5){
                 return <PreviewPost key={index} data={link} creator={coolDude.name} />;
                 }
-              })
+              })}
+              </div>
             ) : (
               <></>
             )}
