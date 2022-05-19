b28f1cf16e80682e69cd097d2affd41f3e5c19b8
Alec Eiber <aleceiber@gmail.com>
2022-05-19 17:46:01 -0400

    updated styling

--- src/components/previewPost.js
+++ src/components/previewPost.js
@@ -19,7 +19,7 @@ const PreviewPost = ({ data, creator }) => {
   }, []);
 
   return (
-    <div className="mb-3 me-5">
+    <div className="mb-2 mobilePost">
       <Card style={{ width: '300px' }}>
         <Card.Img variant="top" src={photo} className="NFTimage" />
         <Card.Body className="text-center">
